aeb8f95a7097e56c4a1bce10a61d7c4ed8dc8375
Nicholas Rodriguez <niko876@hotmail.com>
2019-04-05 17:47:30 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    word_array = @word.split("")
+    anagrams_array = anagrams.each do |words|
+      words.split("")
+    end
+    anagrams_array.each do |pop|
+      if pop.sort == word_array.sort
+        pop.join
+      else
+        pop = []
+      end 
+    end
+  end
+end
